5e35e6475433671e486caa7c6b1afeb58fa1f40f
Alexander Malenkov <malenkov2001@gmail.com>
2024-07-15 18:10:26 +0300

    DIPLOM-100500: Backend fixes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const mysql2 = require('mysql2/promise')
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
 
 const pool = mysql2.createPool({
   host: 'localhost',
@@ -10,13 +9,6 @@ const pool = mysql2.createPool({
   database: 'geoportal_otradnoe',
   password: ''
 })
-
-// получение токена и логина из env
-const access_token = (dotenv?.parsed?.ADS_ACCESS_TOKEN);
-const login = (dotenv?.parsed?.LOGIN);
-
-// объявление origin
-const ADS_ORIGIN = `https://ads-api.ru/main/api?user=${login}&token=${access_token}`;
 
 // инициализация сервера
 const app = express();
@@ -60,115 +52,3 @@ app.get('/advertisements', (req, res) => {
     res.json(data[0])
   })
 });
-
-
-//подключение живого апи (по умолчанию указываем москву и метро отрадное!)
-app.get('/objects-avito', async (req, res) => {
-  try {
-    const response = await fetch(req?._parsedUrl?.search ? `https://ads-api.ru/main/api${req?._parsedUrl?.search}&user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=1` : `https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=1`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-app.get('/objects-cian', async (req, res) => {
-  try {
-    console.log(req?._parsedUrl?.search ? `https://ads-api.ru/main/api${req?._parsedUrl?.search}&user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=4` : `https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=4`)
-    const response = await fetch(req?._parsedUrl?.search ? `https://ads-api.ru/main/api${req?._parsedUrl?.search}&user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=4` : `https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=4`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-app.get('/objects-sob', async (req, res) => {
-  try {
-    const response = await fetch(req?._parsedUrl?.search ? `https://ads-api.ru/main/api${req?._parsedUrl?.search}&user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=5&` : `https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=5`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-app.get('/objects-youla', async (req, res) => {
-  try {
-    const response = await fetch(`https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Владыкино&metro=Отрадное&source=6`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-app.get('/objects-moyareklama', async (req, res) => {
-  try {
-    const response = await fetch(`https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=10`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-app.get('/objects-domclick', async (req, res) => {
-  try {
-    const response = await fetch(`https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=11`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-//irr.ru, n1.ru
-app.get('/objects-archived', async (req, res) => {
-  try {
-    const response = await fetch(`https://ads-api.ru/main/api?user=${login}&token=${access_token}&city=Москва&metro=Отрадное&source=2,7`);
-    if (response.ok) {
-      const data = await response.json();
-      res.json(data?.data);
-    } else {
-      throw new Error('Ошибка при загрузке данных из внешнего API');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Произошла ошибка при загрузке данных из внешнего API');
-  }
-});
-
-
-
